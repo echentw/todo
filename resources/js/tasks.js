35b79ec409a085c2e009ba17b73233b1b79571b5
Eric Chen <echentw@mit.edu>
2017-05-23 23:54:13 -0400

    ellipsis and focus background color

--- resources/js/tasks.js
+++ resources/js/tasks.js
@@ -10,15 +10,16 @@ const TASKS = [
   {'id': 'bbba', 'completed': false, 'text': 'milk the cow for top quality milk'},
   {'id': 'bbbb', 'completed': true, 'text': 'visit chicken farm and acquire super large egg'},
   {'id': 'bbbc', 'completed': false, 'text': 'get extra fine flour from Mill Lane Mill'},
-  {'id': 'bbbd', 'completed': false, 'text': 'something'},
-  {'id': 'bbbe', 'completed': false, 'text': 'something'},
-  {'id': 'bbbf', 'completed': false, 'text': 'something'},
-  {'id': 'bbbg', 'completed': false, 'text': 'something'},
-  {'id': 'bbbh', 'completed': false, 'text': 'something'},
-  {'id': 'bbbi', 'completed': false, 'text': 'something'},
-  {'id': 'bbbj', 'completed': false, 'text': 'something'},
-  {'id': 'bbbk', 'completed': false, 'text': 'something'},
-  {'id': 'bbbl', 'completed': false, 'text': 'something'},
+  {'id': 'bbbd', 'completed': false, 'text': ''},
+  {'id': 'bbbe', 'completed': true, 'text': 'checkbox animation'},
+  {'id': 'bbbj', 'completed': true, 'text': 'strikethrough animation'},
+  {'id': 'bbbf', 'completed': false, 'text': 'choose a better font'},
+  {'id': 'bbbg', 'completed': false, 'text': 'make the colors look better'},
+  {'id': 'bbbh', 'completed': false, 'text': 'make subtasks work'},
+  {'id': 'bbbi', 'completed': false, 'text': 'more info right sidebar?'},
+  {'id': 'bbbk', 'completed': false, 'text': 'turn animations off for initial render'},
+  {'id': 'bbbl', 'completed': true, 'text': 'get rid of blue highlighting when focusing'},
+  {'id': 'bbbm', 'completed': false, 'text': 'think of something cool to build with stella'},
 ];
 
 function ListName(props) {
@@ -38,9 +39,13 @@ function Task(props) {
   const {taskText} = props;
   const {id} = props;
   const {completed} = props;
+  const {focused} = props;
   const {checkHandler} = props;
+  const {focusHandler} = props;
+  const {blurHandler} = props;
 
   const strikethroughClass = completed ? ' strikethrough' : '';
+  const focusClass = focused ? ' focused' : '';
 
   function focusDiv(event) {
     event.target.childNodes[3].focus();
@@ -54,7 +59,7 @@ function Task(props) {
   // &zwnj is a zero-width non-joiner, it is needed to prevent the cursor from
   // behaving unexpectedly when taskText is the empty string.
   return (
-    <div className="task-container">
+    <div className={'task-container' + focusClass}>
       <div className="checkbox-container">
         <input type="checkbox"
                className="checkbox"
@@ -68,11 +73,15 @@ function Task(props) {
         <span className={'strikethrough-able' + strikethroughClass}
               contentEditable={true}
               suppressContentEditableWarning={true}
-              onClick={focusInput}>
+              onClick={focusInput}
+              onFocus={focusHandler}
+              onBlur={blurHandler}>
           {taskText}
         </span>
       </div>
-      <div className="more-info"></div>
+      <div className="more-info">
+        <i className="fa fa-ellipsis-h" aria-hidden="true"></i>
+      </div>
     </div>
   );
 }
@@ -83,9 +92,12 @@ class TaskList extends Component {
     this.state = {
       name: props.name,
       tasks: props.tasks,
+      focus: null,
     };
 
     this.checkHandler = this.checkHandler.bind(this);
+    this.focusHandler = this.focusHandler.bind(this);
+    this.blurHandler = this.blurHandler.bind(this);
   }
 
   checkHandler(event) {
@@ -98,13 +110,27 @@ class TaskList extends Component {
     this.setState({tasks: tasks});
   }
 
+  focusHandler(event) {
+    const id = event.target.parentNode.parentNode.childNodes[0].childNodes[0].getAttribute('id');
+    this.setState({focus: id});
+  }
+
+  blurHandler(event) {
+    const id = event.target.parentNode.parentNode.childNodes[0].childNodes[0].getAttribute('id');
+    this.setState({focus: null});
+  }
+
   render() {
     const tasks = this.state.tasks.map((task) => {
+      const focused = (this.state.focus === task.id);
       return <Task key={task.id}
                    taskText={task.text}
                    id={task.id}
+                   focused={focused}
                    completed={task.completed}
                    checkHandler={this.checkHandler}
+                   focusHandler={this.focusHandler}
+                   blurHandler={this.blurHandler}
              />
     });
     return (
