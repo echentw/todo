fe11818f8ddba6e637c4adcb04f5a3c5d274ed08
Eric Chen <echentw@mit.edu>
2017-05-28 03:59:48 -0400

    make task texts overflow horizontally

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,16 +6,11 @@ const TaskList = require('./tasklist');
 
 const TASKLIST_NAME = 'Tasklist 1';
 const TASKS = [
-  {'completed': true, 'text': 'create new task'},
-  {'completed': true, 'text': 'delete task'},
   {'completed': false, 'text': 'keep track of caret in tasks (when pressing up/down)'},
-  {'completed': true, 'text': 'create new task when pressing enter in the middle of task text'},
-  {'completed': true, 'text': 'backspace at the beginning of a task text'},
-  {'completed': true, 'text': 'string escaping bug'},
-  {'completed': true, 'text': 'pressing left/right at beginning/end of task text'},
   {'completed': false, 'text': 'overflow problem for task list (vertical)'},
-  {'completed': false, 'text': 'overflow problem for long task (horizontal)'},
-  {'completed': true, 'text': 'strikethrough bug'},
+  {'completed': true, 'text': 'overflow problem for long task (horizontal)'},
+  {'completed': false, 'text': 'press enter at beginning of task text'},
+  {'completed': false, 'text': 'minor bug: strikethrough not long enough for long task texts'},
   {'completed': false, 'text': 'choose a better font'},
   {'completed': false, 'text': 'make the colors look better'},
   {'completed': false, 'text': 'make subtasks work'},
